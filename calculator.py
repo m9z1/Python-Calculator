0b4844a85c4765b1a65e38f80375473f30c63b77
... <muzyyyyyyy@gmail.com>
2024-08-29 19:43:12 +0400

    Added new operation(square root)

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
+#Importing Modules
+import math
+
 
 #Main function
 def main():
-    choice = input("Choose Operation: \n 1 - Addition \n 2 - Subtraction \n 3 - Division \n 4 - Multiplication \n 5 - Exponents \n").lower()
+    choice = input("Choose Operation: \n 1 - Addition \n 2 - Subtraction \n 3 - Division \n 4 - Multiplication \n 5 - Exponents \n 6 - Square Root \n").lower()
     
     if choice == '1' or choice == 'addition':
         x = float(input("Enter the first number: "))
@@ -23,6 +26,10 @@ def main():
         x = float(input('Enter a number: '))
         y = int(input('Enter the power: '))
         print(exponents(x, y))
+    
+    elif choice == '6' or choice == 'square root':
+        x = int(input("Enter the number: "))
+        print(sqroot(x))
         
     else:
         print("Error")
@@ -42,5 +49,8 @@ def multiplication(m, n):
     
 def exponents(m, n):
     return m ** n    
+
+def sqroot(m):
+    return math.sqrt(m)
     
 main()
