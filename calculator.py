2e08034763347e7353ac316de6c73568c8dd4388
... <muzyyyyyyy@gmail.com>
2024-08-29 18:52:09 +0400

    Added a new operation (Exponents)

--- calculator.py
+++ calculator.py
@@ -1,19 +1,29 @@
 
 #Main function
 def main():
-    x = float(input("Enter the first number: "))
-    y = float(input("Enter the second number: "))
-    
-    choice = input("Choose Operation: \n 1 - Addition \n 2 - Subtraction \n 3 - Division \n 4 - Multiplication \n").lower()
+    choice = input("Choose Operation: \n 1 - Addition \n 2 - Subtraction \n 3 - Division \n 4 - Multiplication \n 5 - Exponents \n").lower()
     
     if choice == '1' or choice == 'addition':
+        x = float(input("Enter the first number: "))
+        y = float(input("Enter the second number: "))
         print(addition(x, y))
     elif choice == '2' or choice == 'subtraction':
+        x = float(input("Enter the first number: "))
+        y = float(input("Enter the second number: "))
         print(subtraction(x, y))
     elif choice == '3' or choice == 'division':
+        x = float(input("Enter the first number: "))
+        y = float(input("Enter the second number: "))
         print(division(x, y))
     elif choice == '4' or choice == 'multiplication':
+        x = float(input("Enter the first number: "))
+        y = float(input("Enter the second number: "))
         print(multiplication(x, y))
+    elif choice == '5' or choice == 'exponents':
+        x = float(input('Enter a number: '))
+        y = int(input('Enter the power: '))
+        print(exponents(x, y))
+        
     else:
         print("Error")
 
@@ -30,5 +40,7 @@ def division(m, n):
 def multiplication(m, n):
     return m * n
     
+def exponents(m, n):
+    return m ** n    
     
 main()
